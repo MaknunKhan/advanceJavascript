aebad5a414dd3282f981c1d64b6d273c2a92c309
MaknunKhan <maknun101@gmail.com>
2020-04-26 16:39:37 +0600

    truty false concept

---
+++ advance.js
@@ -0,0 +1,52 @@
+//falsy value
+
+
+//0
+// ""
+// let name = 0;
+// let name = "";
+// let name = undefined;
+// let name = null;
+// let name = NaN;
+
+
+
+
+// truthy value
+// if number is define either it positive  or negative it is truthy value
+// let name = 1;
+
+
+// let name = " "    it is also truthy value if there have any space in string then javascript think there have value .
+
+// let name = true; it define as a true
+//  let name = [] it will truthy value reason its array
+ 
+// let name = {};  it it same like array
+
+
+
+
+
+
+
+// if( name){
+//     console.log("condition is true");
+// }else{  
+//     console.log("Condition is  false");
+// }
+
+
+//  any number is Truthy other than 0 ... any string is truthy other than Empty String
+// const name = "0";
+let name = false;
+console.log(name);
+
+
+
+if(name){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
