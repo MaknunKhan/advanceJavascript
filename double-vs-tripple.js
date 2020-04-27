7fda03c3b156d63b89dce2010c67d1d53ddff2ab
MaknunKhan <maknun101@gmail.com>
2020-04-27 15:51:28 +0600

    done

---
+++ double-vs-tripple.js
@@ -0,0 +1,100 @@
+/*const first = 2;
+const second = 3;
+
+if(first == second){
+    console.log("Conditions is true");
+}else{
+    console.log("Conditions is false");
+}
+*/
+/*
+const one = 1;
+const first = 1;
+ if(one == first){
+     console.log("Conditions is true");
+ }else{
+     console.log("conditions is false");
+ }
+ */
+
+
+
+/*
+ const first = 2; // this is number
+ const two = "2"; // this is string
+
+if(first == two){
+    console.log("Condition is True");
+}else{
+    console.log("condition is false");
+}
+// Still output will be true == do not care about type 
+// 2 == "2" it will same in double equal   it does not check which one which type
+
+//  == it will check only the value
+
+*/
+
+/*
+const first  = 2;
+const second = true;
+
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+// in javascript  if you tell its true it will compare with 1
+// in Javascript true == 1;
+/*
+const first  = 1;
+const second = true;
+
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+*/
+
+
+// in javascript if you tell its false it will compare with 0
+
+// false == 0; in double equal 
+
+
+const one = 0;
+const two = false;
+
+if(one == two){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+
+
+
+
+
+/// ===  it will check strictly
+// === it will check value and the type
+/*
+const first = 2;
+const second = "2";
+
+if(first === second){ 
+    console.log("condition is true");
+}else{
+    console.log("Condition is false");
+}
+*/
+
+//// here === tripple equal is identify type which one is number or which one string then give output .. 
