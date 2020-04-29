75ae3010d3db3ce7d1c1b8f6ef8d76ee363ba1f2
MaknunKhan <maknun101@gmail.com>
2020-04-29 16:43:47 +0600

    done

---
+++ arguments.js
@@ -0,0 +1,14 @@
+function add (num1, num2){
+   const arg = [...arguments];
+    for(let i =0; i<add.length; i++){
+        const num = add[i];
+        
+        const total = num + num;
+    }
+
+    
+   
+
+}
+const result = add(10,20,60,40);
+console.log(result);
---
+++ scope.js
@@ -0,0 +1,21 @@
+
+let bonus = 30;
+
+
+function add(num,num2){
+    const result = num + num2 + bonus;
+  
+    if( result > 9){
+       var mood = 'king'
+    
+      
+    }
+    console.log(m);
+    var m = "month";
+ 
+
+   
+   console.log(mood);
+}
+
+const total = add(2,4);
