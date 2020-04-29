4350a112318b91893aa76a73f157dc858bd970b4
MaknunKhan <maknun101@gmail.com>
2020-04-29 14:59:40 +0600

    done

---
+++ map-filter.js
@@ -0,0 +1,70 @@
+// const numbers = [1,3,4,4,6];
+// const output = [];
+
+
+// for(let i = 0; i < numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+
+
+// }
+// console.log(output);
+
+const numbers = [2,5,6,6,67,7];
+
+// function square (element){
+//     return element * element;
+
+// }
+
+//  const result = numbers.map( function(element){
+//     return element * element;
+
+// });
+// console.log(result);
+
+//  const add = numbers.map(function(element){
+//     return element + element;
+    
+// });
+
+// console.log(add);
+
+// const minus = numbers.map(function(element){
+//     return element - element;
+// });
+// console.log(minus);
+
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+// const add =  numbers.map(x => x + x);
+// console.log(add);
+
+// const division = numbers.map(x => x / x );
+// console.log(division);
+
+
+// const minus = numbers.map(x => x - x);
+// console.log(minus);
+
+// const bigger = numbers.filter(x => x > 5);
+// console.log(bigger);
+// const little = numbers.filter( x => x < 5);
+// console.log(little);
+// const equal = numbers.filter( x => x >= 5);
+// console.log(equal);
+
+
+const isThere = numbers.find( x => x > 5);
+console.log(isThere);
+const isLittle = numbers.find(x => x < 5);
+console.log(isLittle);
+
+
+
+
+
+
